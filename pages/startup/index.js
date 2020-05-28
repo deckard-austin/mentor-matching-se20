a1d814f69cb4d6f19c0aeaf8bb19bdce653c7de7
deckard-austin <deckardaustin852@gmail.com>
2020-05-27 18:30:48 -0600

    comments in startups index

--- pages/startup/index.js
+++ pages/startup/index.js
@@ -8,12 +8,28 @@ const fetcher = (API_URL) => fetch(API_URL).then((r) => r.json());
 const MentorList = () => {
   const { data, error, isValidating, mutate } = useSWR(API_URL, fetcher);
   console.log({ data });
+
+  // base('Mentors')
+  //   .select({
+  //     view: 'Mentors',
+  //   })
+  //   .firstPage(function (err, records) {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     records.forEach(function (record) {
+  //       console.log('Retrieved', record.get('Name'));
+  //     });
+  //   });
+
   // if (isValidating) return <div>Fetching data...</div>;
   // // If an error occured, show an error message
   // if (error) return <div>Failed to load.</div>;
   // // If there were no mentors found, show a message that says so
   // if (data.length === 0) return <div>No mentors found.</div>;
   // // Otherwise return a list of mentors and a button to manually refetch the mentors.
+
   return (
     <div>
       <div>
